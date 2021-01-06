4645e7b1d4ae0cd086ceffc5f1995b26b0e21bd4
huozhi <inbox@huozhi.im>
2021-01-06 23:37:30 +0800

    export function preset

--- index.js
+++ index.js
@@ -1,26 +1,28 @@
-module.exports = {
-  presets: [
-    require.resolve('@babel/preset-react'),
-    [
-      require.resolve('@babel/preset-env'),
-      {
-        loose: true,
-        useBuiltIns: false,
-        targets: {
-          node: '4',
-          esmodules: true,
+module.exports = () => {
+  return {
+    presets: [
+      require.resolve("@babel/preset-react"),
+      [
+        require.resolve("@babel/preset-env"),
+        {
+          loose: true,
+          useBuiltIns: false,
+          targets: {
+            node: "4",
+            esmodules: true,
+          },
         },
-      },
+      ],
     ],
-  ],
-  plugins: [
-    [
-      require.resolve('@babel/plugin-proposal-class-properties'),
-      {loose: true}
+    plugins: [
+      [
+        require.resolve("@babel/plugin-proposal-class-properties"),
+        { loose: true },
+      ],
+      [
+        require.resolve("@babel/plugin-proposal-optional-chaining"),
+        { loose: true },
+      ],
     ],
-    [
-      require.resolve('@babel/plugin-proposal-optional-chaining'),
-      {loose: true}
-    ]
-  ]
- }
+  };
+};
