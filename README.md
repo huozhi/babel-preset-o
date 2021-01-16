d6f363b471e94f19042aa0a6ed269075f25d8403
Jiachi Liu <inbox@huozhi.im>
2021-01-17 01:13:00 +0800

    feature: add react options + doc

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 > tiny and flexible babel preset
 
 cover basic usage for react development
-### Usage
+## Usage
 
 ```sh
 npm i -S babel-preset-o
@@ -12,5 +12,31 @@ babel.config.js
 ```js
 module.exports = {
   presets: ['babel-preset-o']  
+}
+```
+
+## Options
+Use this preset with customized options to extend flexibility of compilation.
+#### `nodeVersion`
+
+Default is `undefined`, will use browserslist if project specified. Ref to [babel-preset-env#targetsnode](https://babeljs.io/docs/en/babel-preset-env#targetsnode)
+#### `modules`
+
+Default is `false`, ref to [babel-preset-env#modules](https://babeljs.io/docs/en/babel-preset-env#modules)
+#### `useBuiltIns`
+
+Default is `false`, ref to [babel-preset-env#usebuiltins](https://babeljs.io/docs/en/babel-preset-env#usebuiltins)
+#### `runtime`
+
+Default is `'classic'`, Ref to [babel-preset-react#runtime](https://babeljs.io/docs/en/babel-preset-react#runtime)
+
+### Example
+
+```js
+// babel.config.js
+module.exports = {
+  presets: [
+    ['babel-preset-o', { modules: 'commonjs', nodeVersion: '8' }]
+  ]  
 }
 ```
--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const path = require('path')
 
 module.exports = (api, options = {}) => {
   const nodeEnv = process.env.NODE_ENV
+  const isTest = nodeEnv === 'test'
   const isDev = nodeEnv === 'development'
+  const isProd = nodeEnv === 'production'
 
   let absoluteRuntime = null
   try {
@@ -12,31 +14,46 @@ module.exports = (api, options = {}) => {
   } catch (_) {}
 
   const {
-    nodeVersion = 'current',
     modules = false,
     useBuiltIns = false,
+    runtime = 'classic',
+    nodeVersion,
   } = options
 
   return {
     presets: [
-      require("@babel/preset-react").default,
       [
-        require("@babel/preset-env").default,
+        require('@babel/preset-react').default,
+        {
+          ...(runtime !== 'automatic' ? { useBuiltIns: true } : {}),
+          runtime,
+        }
+      ],
+      isTest && [
+        require('@babel/preset-env').default,
+        {
+          targets: { node: 'current' }
+        }
+      ],
+      isProd && [
+        require('@babel/preset-env').default,
         {
           loose: true,
           corejs: useBuiltIns ? 3 : false,
           modules,
           useBuiltIns,
-          targets: {
-            node: nodeVersion,
-          },
+          // when node version is specified, use nodeVersion first,
+          // otherwise let browserslist works
+          ...(nodeVersion ? {
+            targets: { node: nodeVersion }
+          } : {}),
           exclude: ['@babel/plugin-transform-typeof-symbol'],
         },
       ],
-    ],
+    ].filter(Boolean),
     plugins: [
       [
-        require("@babel/plugin-proposal-class-properties").default,
+        require('@babel/plugin-proposal-class-properties').default,
         { loose: true },
       ],
       isDev && [
@@ -51,11 +68,11 @@ module.exports = (api, options = {}) => {
         }
       ],
       [
-        require("@babel/plugin-proposal-optional-chaining").default,
+        require('@babel/plugin-proposal-optional-chaining').default,
         { loose: true },
       ],
       [
-        require("@babel/plugin-proposal-nullish-coalescing-operator").default,
+        require('@babel/plugin-proposal-nullish-coalescing-operator').default,
         { loose: true },
       ]
     ].filter(Boolean),
